348724556627e213181d5011583d28c06c26e623
Diego Isai Salazar Rico <diego.salazar@encora.com>
2025-02-25 18:52:48 -0600

    fix: removed unused imports in AutController

--- src/main/java/com/bt2/spotify_consumer/controller/AuthController.java
+++ src/main/java/com/bt2/spotify_consumer/controller/AuthController.java
@@ -3,9 +3,7 @@ package com.bt2.spotify_consumer.controller;
 import com.bt2.spotify_consumer.service.AuthServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 
