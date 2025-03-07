9aa756d85e45b3b8a282f43c3ba02326b83d9a71
Diego Isai Salazar Rico <diego.salazar@encora.com>
2025-03-07 09:40:35 -0600

    feat: fetch top artist with token refresh

--- src/main/java/com/bt2/spotify_consumer/service/AuthService.java
+++ src/main/java/com/bt2/spotify_consumer/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.bt2.spotify_consumer.service;
 
 import com.bt2.spotify_consumer.config.SpotifyClientConfiguration;
+import com.bt2.spotify_consumer.exception.UnauthorizedException;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -10,6 +11,8 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import javax.print.attribute.standard.Media;
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
@@ -40,5 +43,29 @@ public class AuthService implements AuthServiceInterface {
         ResponseEntity<Map> response = restTemplate.postForEntity(spotifyConfig.getSpotifyTokenUrl(), request, Map.class); //url, request, response type
 
         return response.getBody();
+    }
+
+    public Map<String, String> refreshAccessToken(String refreshToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.setBasicAuth(spotifyConfig.getClientId(), spotifyConfig.getClientSecret()); // Oauth requests basic auth on headers
+
+        // Create http body
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "refresh_token");
+        params.add("refresh_token", refreshToken);
+
+        // Create http request
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, String> responseBody = restTemplate.postForEntity(spotifyConfig.getSpotifyTokenUrl(), request, Map.class).getBody(); //url, request, response type
+        if (responseBody != null && responseBody.containsKey("refresh_token") && responseBody.containsKey("access_token")) {
+            Map<String, String> tokens = new HashMap<>();
+            tokens.put("access_token", responseBody.get("access_token"));
+            tokens.put("refresh_token", responseBody.get("refresh_token"));
+            return tokens;
+        }
+
+        throw new UnauthorizedException("Invalid token");
     }
 }
